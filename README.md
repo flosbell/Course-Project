321f20d51015bfa9d07d386d852f965e04de106e
flosbell <daniela.kuruczova@gmail.com>
2014-07-25 12:05:12 +0200

    V 0.5.2

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
 Course project - Getting and Cleaning Data, Summer 2014
-
+=======================================================
 
 CONTENTS OF GITHUB FOLDER
-
-README.md 	--> This file
-
-codebook.md 	--> Description of the tidy dataset
-
-run_analysis.R 	--> R script that produces the file 'tidy.txt'
+--------------------------
+  * README.md 	--> This file
+  * codebook.md 	--> Description of the tidy dataset
+  * run_analysis.R 	--> R script that produces the file 'tidy.txt'
 
 
 ABOUT run_analysis.R
-
+---------------------
  The script will download and unzip dataset into the working directory and produce file 'tidy.txt'
 
 
 REQUIREMENTS FOR run_analysis.R
-
- Distribution of R
- Installed data.table package
- Internet Connection	
+--------------------------------
+ * Distribution of R
+ * Installed data.table package
+ * Installed tools package
+ * Internet Connection	
 
 
 HOW TO USE run_analysis.R
-
- 1. Set the directory that will contain tidy.txt as the working directory.
- 2. Save the file 'run_analysis.R' in this directory
- 3. Type 'source("run_analysis.R")' into the console and press enter
- 4. Wait while the script works (can take up to several minutes, depending on your inetrnet connection and PC)
+-------------------------------
+ 1. Run R studio or R console
+ 2. Save the file 'run_analysis.R' into directory that you want to work in
+ 3. Set the directory containing 'run_analysis.R' as the working directory.
+ 4. Type 'source("run_analysis.R")' into the console and press enter
+ 5. Wait while the script works (can take up to several minutes, depending on your inetrnet connection and PC)
  5. After the script finishes, file 'tidy.txt' should be in your working directory. You can load it
     into R using the 'read.table("tidy.txt")' command.
--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,25 @@
-#Dowload and unzip data
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileURL,destfile="data.zip")
-unzip("data.zip")
+library(data.table)
+library(tools)
+
+message("Checking working directory for data.zip")
+filesum <- as.vector(md5sum("data.zip"))
+
+if (is.na(filesum) || filesum != "d29710c9530a31f303801b6bc34bd895") {
+  #Dowload and unzip data
+  message("Downloading data")
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL,destfile="data.zip")
+  unzip("data.zip")
+} else {message("data.zip found in the working directory")}
+
 #Load the variable labels
+message("Loading variable labels")
 labels <- readLines("./UCI HAR Dataset/features.txt")
 labels <- c("Subject","Activity","Dataset",labels)
-activities <- scan("./UCI HAR Dataset/activity_labels.txt",what="character")
+activities <- scan("./UCI HAR Dataset/activity_labels.txt",what="character",quiet=T)
 activities <- activities[seq(from=2,to=12,by=2)]
 #Load the train data
+message("Loading train data")
 train_data <- read.table("./UCI HAR Dataset/train/X_train.txt", header=F,sep="")
 train_subject <- readLines("./UCI HAR Dataset/train/subject_train.txt")
 train_labels <- readLines("./UCI HAR Dataset/train/y_train.txt")
@@ -16,6 +28,7 @@ for (i in 1:6){
   train_labels[train_labels==i] <- rep(activities[i],length(train_labels[train_labels==i]))
 }
 #Load the test data
+message("Loading test data")
 test_data <- read.table("./UCI HAR Dataset/test/X_test.txt", header=F,sep="")
 test_subject <- readLines("./UCI HAR Dataset/test/subject_test.txt")
 test_labels <- readLines("./UCI HAR Dataset/test/y_test.txt")
@@ -24,12 +37,12 @@ for (i in 1:6){
   test_labels[test_labels==i] <- rep(activities[i],length(test_labels[test_labels==i]))
 }
 
+message("Processing data")
 #Create label vectors to signify dataset of origin
 train_lab <- rep("TRAIN",length(train_subject))
 test_lab <- rep("TEST",length(test_subject))
 
 #Load the data into data table
-library(data.table)
 TRAIN <- data.table(train_subject,train_labels,train_lab,train_data)
 TEST <- data.table(test_subject,test_labels,test_lab,test_data)
 #Merge both datasets
@@ -40,12 +53,14 @@ filter <- sort(c(1,2,3,grep("mean()",labels,fixed=T),grep("std()",labels,fixed=T
 DATASET <- DATA[,filter,with=F]
 labels <-labels[filter]
 
+
 #Create tidy dataset
 setkey(DATASET,train_subject,train_labels)
 TIDY <- DATASET[,lapply(.SD,mean),by="train_subject,train_labels,train_lab"]
 #Set variable names for the tidy dataset
 setnames(TIDY,labels)
 #Export the tidy dataset to file
+message("Creating tidy.txt")
 write.table(TIDY, "tidy.txt", sep="\t")
 
 #Remove all created objects from workspace
@@ -66,6 +81,7 @@ rm(test_subject)
 rm(test_labels)
 rm(train_lab)
 rm(test_lab)
-
+rm(filesum)
+message("DONE!")
 #Load the tidy data into the workspace
-tidy_data <- read.table("tidy.txt", header=T,sep="\t")
+#tidy_data <- read.table("tidy.txt", header=T,sep="\t")
